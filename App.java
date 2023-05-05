f21460bffef67f3bff5dc5168a47822a66bef869
BouilleJames <77192627+BouilleJames@users.noreply.github.com>
2023-05-05 12:42:20 +0200

    add

--- App.java
+++ App.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        Chat c = new Chat("Tom", 1, true, "Miaou");
-        name(c);
+        Chat c = new Chat("Tom", 1, true, "Miaou"); // instance de la classe Chat
+        c.setmVaccin(false);
+        name(c); // UN APPEL à une fonction ou methode
+        
         // Chat d = new Chat("Felix", 5, false, "Miaou");
         // name(d);
     }
 
+    // une fonction a un nom et entre parentheses des parametres et entre accolades le code
     public static void name(Chat chat) {
         String question = "Bonjour je m'appelle " + chat.getName() + " je suis un chat de " + chat.getAge() + "ans" + (chat.getVaccin() ? " je suis vacciné!" : " je ne suis pas vacciné!") + "Voulez-vous que je miaule ? (y/n)";
         boolean continuePresentation = true;
